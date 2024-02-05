1d0860729ca11111ebf7c42275421aec115de720
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-02-05 23:11:59 +0530

    Practice

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -41,7 +41,8 @@ const books = [
   let userBooks = books.filter( (bk) => bk.genre === 'Non-Fiction')
 
   userBooks = books.filter( (bk) => { 
-    return bk.publish >= 1980 && bk.genre === "Non-Fiction"
+    // return bk.publish >= 1980 && bk.genre === "Non-Fiction"
+    return bk.publish >= 2000 || bk.genre === "History"
 })
 
   console.log(userBooks);
