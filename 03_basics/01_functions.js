39c6ec836f97ba550cedd939c0f055fe1fba2845
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-05-04 22:52:40 +0530

    update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,6 +22,8 @@ function addTwoNumbers(number1, number2){
     return number1 + number2
 }
 
+
+
 const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
@@ -65,7 +67,6 @@ const myNewArray = [200, 400, 100, 600]
 function returnSecondValue(getArray){
     return getArray[1]
 }
-
 
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
